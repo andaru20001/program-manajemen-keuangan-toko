59c98078c3a4c78339aefe7b3c81eb8ea001cec6
irka021002 <irfankamal021002@gmail.com>
2021-05-18 22:15:34 +0700

    final

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends BaseController
 {
 	public function index(){
 		$db = \Config\Database::connect();
-		$dataTabel = $db->query("SELECT tgl_transaksi, keterangan_transaksi, jmlh_transaksi, nama_rekening, jenis_transaksi from transaksi INNER JOIN rekening using(id_rekening);");
+		$dataTabel = $db->query("SELECT id_transaksi, tgl_transaksi, keterangan_transaksi, jmlh_transaksi, nama_rekening, jenis_transaksi from transaksi INNER JOIN rekening using(id_rekening);");
 		$rekening = $db->query("SELECT nama_rekening, saldo_rekening FROM rekening;");
 		$produk = $db->query("SELECT nama_produk FROM produk;");
 		$supplier = $db->query("SELECT nama_supplier FROM supplier;");
@@ -74,6 +74,69 @@ class Home extends BaseController
 		$db = \Config\Database::connect();
 		$db->query("INSERT INTO produk (nama_produk) VALUES ('$nama');");
 		$db->query("INSERT INTO produk (harga_produk) VALUES ('$harga');");
+
+		return redirect()->to('/Home');
+	}
+
+	public function delete_trans($id){
+		$db = \Config\Database::connect();
+		$transaksi = $db->query("SELECT id_rekening, jmlh_transaksi, jenis_transaksi FROM transaksi WHERE id_transaksi = '$id';");
+		$idRek = NULL;
+		$jmlhTrx = NULL;
+		$jenisTRX = NULL;
+		foreach($transaksi->getResultArray() as $trx){
+			$idRek = $trx["id_rekening"];
+			$jmlhTrx = $trx["jmlh_transaksi"];
+			$jenisTRX = $trx["jenis_transaksi"];
+		}
+		$rekening = $db->query("SELECT saldo_rekening FROM rekening WHERE id_rekening = '$idRek';");
+		$saldoRek = NULL;
+		foreach($rekening->getResultArray() as $rek){
+			$saldoRek = $rek["saldo_rekening"];
+		}
+
+		if($jenisTRX === "Pengeluaran" ){
+			$saldo_akhir = $jmlhTrx + $saldoRek;
+			$db->query("UPDATE rekening SET saldo_rekening = '$saldo_akhir' WHERE id_rekening = '$idRek';");
+		}else{
+			$saldo_akhir = $saldoRek - $jmlhTrx;
+			$db->query("UPDATE rekening SET saldo_rekening = '$saldo_akhir' WHERE id_rekening = '$idRek';");
+		}
+
+		$db->query("DELETE FROM transaksi WHERE id_transaksi = '$id';");
+		return redirect()->to('/Home');
+	}
+
+	public function edit_trans($id){
+		$tgl = isset($_POST["tgl"]) ? $_POST["tgl"] : NULL;
+		$ket = isset($_POST["ket"]) ? $_POST["ket"] : NULL;
+		$rek = isset($_POST["rek"]) ? $_POST["rek"] : NULL;
+		$prd = isset($_POST["prd"]) ? $_POST["prd"] : NULL;
+		$spl = isset($_POST["spl"]) ? $_POST["spl"] : NULL;
+		$sbt = isset($_POST["sbt"]) ? $_POST["sbt"] : NULL;
+		$jns = isset($_POST["jns"]) ? $_POST["jns"] : NULL;
+
+		$db = \Config\Database::connect();
+		$db->query("UPDATE transaksi SET tgl_transaksi = '$tgl', jmlh_transaksi = '$sbt', keterangan_transaksi='$ket', jenis_transaksi = '$jns', id_produk = (SELECT id_produk FROM produk WHERE nama_produk='$prd'), id_supplier = (SELECT id_supplier FROM supplier WHERE nama_supplier='$spl'),id_rekening = (SELECT id_rekening FROM rekening WHERE nama_rekening='$rek') WHERE id_transaksi = '$id';");
+
+		// INI BLOK KODE UPDATE SALDO REKENING, BISA DIPAKE DI FUNGSI EDIT DAN DELETE NANTI
+		$msk = $db->query("SELECT jmlh_transaksi FROM transaksi INNER JOIN rekening WHERE jenis_transaksi='Pemasukan' AND nama_rekening='$rek'");
+		$klr = $db->query("SELECT jmlh_transaksi FROM transaksi INNER JOIN rekening WHERE jenis_transaksi='Pengeluaran' AND nama_rekening='$rek'");
+		$pemasukan = 0;
+		$pengeluaran = 0;
+		foreach($msk->getResultArray() as $mskval){
+			$backup1 = $pemasukan;
+			$pemasukan = $backup1 + $mskval['jmlh_transaksi'];
+		}
+		foreach($klr->getResultArray() as $klrval){
+			$backup2 = $pengeluaran;
+			$pengeluaran = $backup2 + $klrval['jmlh_transaksi'];
+		}	
+		
+		$jml = $pemasukan - $pengeluaran;
+
+		$db->query("UPDATE rekening SET saldo_rekening='$jml' WHERE nama_rekening='$rek'");
+		// AKHIR DARI BLOK KODE UPDATE SALDO REKENING
 
 		return redirect()->to('/Home');
 	}
--- app/Views/home.php
+++ app/Views/home.php
@@ -53,12 +53,82 @@
                     <td><?php echo $dt['nama_rekening'] ;?></td>
                     <td><?php echo $dt['jenis_transaksi'] ;?></td>
                     <td><?php $convert = number_format($dt['jmlh_transaksi'], 2, '.', ','); echo "Rp. " . $convert ;?></td>
-                    <td><span class="iconify" data-icon="ant-design:edit-outlined" data-inline="false"></span> <span class="iconify" data-icon="fluent:delete-20-filled" data-inline="false"></span></td>
+                    <td><button type="button" data-bs-toggle="modal" data-bs-target="#edit-modal-<?php echo $dt["id_transaksi"]?>" class="edit-button"><span class="iconify" data-icon="ant-design:edit-outlined" data-inline="false"></button> </span> <a href="/Home/delete_trans/<?php echo $dt["id_transaksi"]?>"><span class="iconify" data-icon="fluent:delete-20-filled" data-inline="false"></span></a></td>
                   </tr>
                   <?php endforeach; ?>
                 </tbody>
               </table>
         </div>
     </section>
+
+    <?php foreach($dataTabel->getResultArray() as $dt) : ?>
+        <div class="modal fade" id="edit-modal-<?php echo $dt["id_transaksi"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Transaksi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form action="/Home/edit_trans/<?php echo $dt["id_transaksi"] ?>" method="POST">
+                  <?php echo csrf_field(); ?>
+                  <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Tanggal</label>
+                      <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="tgl">
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleFormControlTextarea1" class="form-label">Keterangan</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="ket"></textarea>
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleFormControlTextarea1" class="form-label">Rekening</label>
+                      <select class="form-select" aria-label="Default select example" name="rek">
+                          <option selected>--Pilih Rekening--</option>
+                          <?php $i = 1; foreach ($rek->getResultArray() as $reken) : ?>
+                          <option value="<?php echo $reken["nama_rekening"] ?>"><?php echo $reken['nama_rekening'] ?></option>
+                          <?php endforeach; ?>
+                      </select>
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Produk (Untuk pemasukan, kosongkan jika pengeluaran)</label>
+                      <!-- <input type="text" class="form-control" id="exampleFormControlInput1" name="idp"> -->
+                      <select class="form-select" aria-label="Default select example" name="prd">
+                          <option selected>--Pilih Produk--</option>
+                          <?php $i = 1; foreach ($prod->getResultArray() as $prd) : ?>
+                          <option value="<?php echo $prd["nama_produk"] ?>"><?php echo $prd['nama_produk'] ?></option>
+                          <?php endforeach; ?>
+                      </select>
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Supplier (Untuk pengeluaran, kosongkan jika pemasukan)</label>
+                      <!-- <input type="text" class="form-control" id="exampleFormControlInput1" name="ids"> -->
+                      <select class="form-select" aria-label="Default select example" name="spl">
+                          <option selected>--Pilih Supplier--</option>
+                          <?php $i = 1; foreach ($supl->getResultArray() as $spl) : ?>
+                          <option value="<?php echo $spl["nama_supplier"];?>"><?php echo $spl['nama_supplier'] ?></option>
+                          <?php endforeach; ?>
+                      </select>
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Jenis Transaksi</label>
+                      <!-- <input type="text" class="form-control" id="exampleFormControlInput1" name="ids"> -->
+                      <select class="form-select" aria-label="Default select example" name="jns">
+                          <option>--Pilih Jenis Transaksi--</option>
+                          <option value="Pemasukan">Pemasukan</option>
+                          <option value="Pengeluaran">Pengeluaran</option>
+                      </select>
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Sub Total</label>
+                      <input type="number" class="form-control" id="exampleFormControlInput1" name="sbt">
+                  </div>
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
+      </div>
+  <?php endforeach; ?>
     
 <?php echo $this->endSection(); ?>
